d6ba476325d5b8adde3113076611f2c838c67aa1
roboli <robertooliveros@mac.com>
2024-03-07 10:16:36 -0600

    Improve main

--- bin/lox_dart.dart
+++ bin/lox_dart.dart
@@ -2,41 +2,43 @@ import 'dart:io';
 import 'package:lox_dart/lox_dart.dart';
 
 void main(List<String> arguments) {
-  if (arguments.isEmpty) {
-    runLine();
+  if (arguments.length > 1) {
+    stdout.writeln('Usage: dart run lox_dart [file]');
+    exit(64);
+  } else if (arguments.length == 1) {
+    runFile(arguments[0]);
   } else {
-    run(arguments[0]);
+    runRepl();
   }
 }
 
-void runLine() {
+void runRepl() {
   stdout.write('> ');
   var input = stdin.readLineSync();
 
   while (input != null) {
-    final scanner = Scanner(input);
-    List<Token> tokens = scanner.scan();
-    for (var token in tokens) {
-      print(token);
-    }
-
-    if (scanner.errors.isNotEmpty) {
-      for (var error in scanner.errors) {
-        print(error.description);
-      }
-    }
-
+    run(input);
     stdout.write('> ');
     input = stdin.readLineSync();
   }
 }
 
-void run(String filename) {
+void runFile(String filename) {
   File(filename).readAsString().then((input) {
-    final scanner = Scanner(input);
-    List<Token> tokens = scanner.scan();
-    for (var token in tokens) {
-      print(token);
-    }
+    run(input);
   });
+}
+
+void run(String input) {
+  final scanner = Scanner(input);
+  List<Token> tokens = scanner.scan();
+  for (var token in tokens) {
+    print(token);
+  }
+
+  if (scanner.errors.isNotEmpty) {
+    for (var error in scanner.errors) {
+      print(error.description);
+    }
+  }
 }
