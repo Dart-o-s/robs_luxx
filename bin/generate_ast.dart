6efad2d96a8e566e242d0899a7f46f3b84d040e5
roboli <robertooliveros@mac.com>
2024-03-08 05:52:17 -0600

    Generate Expr classes

---
+++ bin/generate_ast.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  if (arguments.length != 1) {
+    print('Usage: generate_ast <output directory>');
+    exit(64);
+  }
+
+  String outputDir = arguments[0];
+
+  defineAst(outputDir, "Expr", [
+    "Binary: Expr left, Token operator, Expr right",
+    "Grouping: Expr expression",
+    "Literal: Object value",
+    "Unary: Token operator, Expr right",
+  ]);
+}
+
+void defineAst(String outputDir, String baseName, List<String> types) {
+  String path = '$outputDir/${baseName.toLowerCase()}.dart';
+  final file = File(path);
+  final sink = file.openWrite();
+  sink.writeln('import \'package:lox_dart/token.dart\';');
+  sink.writeln();
+  sink.writeln('abstract class $baseName {');
+  sink.writeln('  T accept<T>(Visitor<T> visitor);');
+  sink.writeln('}');
+  sink.writeln();
+  defineVisitor(sink, baseName, types);
+  sink.writeln();
+
+  for (String type in types) {
+    String className = type.split(':')[0].trim();
+    Iterable<String> fieldList =
+        type.split(':')[1].trim().split(',').map((field) => field.trim());
+    defineType(sink, baseName, className, fieldList);
+    sink.writeln();
+  }
+
+  sink.close();
+}
+
+void defineVisitor(IOSink sink, String baseName, List<String> types) {
+  sink.writeln('abstract class Visitor<T> {');
+
+  for (String type in types) {
+    String typeName = type.split(':')[0].trim();
+    sink.writeln(
+        '  T visit$typeName$baseName($typeName ${baseName.toLowerCase()});');
+  }
+
+  sink.writeln('}');
+}
+
+void defineType(IOSink sink, String baseName, String className,
+    Iterable<String> fieldList) {
+  sink.writeln('class $className extends $baseName {');
+
+  List<String> params =
+      fieldList.map((element) => 'this.${element.split(' ')[1]}').toList();
+  sink.writeln('  $className(${params.join(',')});');
+
+  for (String field in fieldList) {
+    sink.writeln('  final $field;');
+  }
+
+  sink.writeln();
+  sink.writeln('  @override');
+  sink.writeln('  T accept<T>(Visitor<T> visitor) {');
+  sink.writeln('    return visitor.visit$className$baseName(this);');
+  sink.writeln('  }');
+
+  sink.writeln('}');
+}
